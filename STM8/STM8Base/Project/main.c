7741c9cfd667456bef0dbdd758b4509187dad3ec
xinshaochen <942266575@qq.com>
2016-05-18 14:02:18 +0800

    stm8 base

--- STM8/STM8Base/Project/main.c
+++ STM8/STM8Base/Project/main.c
@@ -6,6 +6,9 @@
 #include "remote.h"
 #include "led.h"
 #include "motor.h"
+#include "ADC.h"
+#include "eeprom.h"
+
 
 
 
@@ -24,148 +27,41 @@ int main(void)
 	u8 key;
 	u8 s=0;
   u8 rx_buff[33];
+  u16 t=0;
+  u16 a=0;
+  u16 buf;
       All_Congfig();
-      NRF24L01_Init();
-      while(NRF24L01_Check());
-      NRF24L01_TX_Mode();
-//	  Remote_Init();
-          TIM4_Init();
-          MotorInit();
-//          LED_Init();
-//          LED_R_SET;
-//            LED_G_SET;
-//              LED_B_SET;
-//while(1)
-//{
-//	LED_R_SET;
-//	LED_G_SET;
-//	LED_B_SET;
-//	delay_ms(300);
-//	LED_R_CLR;
-//	LED_B_CLR;
-//	LED_G_CLR;
-//	delay_ms(300);
-//}
-//	  while (1)
-//	  {
-//		  key = Remote_Scan();
-//		  if (key)
-//		  {
-//			  switch (key)
-//			  {
-//			  case 0://str = "ERROR"; 
-//				  break;
-//			  case 162://str = "POWER"; 
-//				  break;
-//			  case 98://str = "UP"; 
-//				  break;
-//			  case 2://str = "PLAY"; 
-//				  break;
-//			  case 226://str = "ALIENTEK"; 
-//				  break;
-//			  case 194://str = "RIGHT"; 
-//				  break;
-//			  case 34://str = "LEFT"; 
-//				  break;
-//			  case 224://str = "VOL-"; 
-//				  break;
-//			  case 168://str = "DOWN"; 
-//				  break;
-//		  case 144://str = "VOL+"; 
-//				  break;
-//			  case 104://str = "1"; 
-//                            if(RmtCnt==0)
-//                            {
-//				  s = !s;
-//				  if (s) LED_R_SET;
-//				  else LED_R_CLR;
-//                            }     
-//				  break;
-//			  case 152://str = "2";
-//				  s = !s;
-//				  if (s) LED_G_SET;
-//				  else LED_G_CLR;
-//                                  
-//				  break;
-//			  case 176://str = "3";
-//				  s = !s;
-//				  if (s) LED_B_SET;
-//				  else LED_B_CLR;
-//                                  
-//				  break;
-//			  case 48://str = "4"; 
-//				  break;
-//			  case 24://str = "5";
-//				  break;
-//			  case 122://str = "6";
-//				  break;
-//			  case 16://str = "7"; 
-//				  break;
-//			  case 56://str = "8";
-//				  break;
-//			  case 90://str = "9";
-//				  break;
-//			  case 66://str = "0";
-//				  break;
-//			  case 82://str = "DELETE";
-//				  break;
-//			  }
-//		  }else 
-//                    delay_ms(10);
-//
-//			  //delay_ms(300);
-//
-//	  }
-			  //RL = 5;
-			  //GL = 19;
-			  //BL = 10;
-			  //while (1);
+  ADC_Init(Channel_6);
+  LED_Init();
+  LED_R_CLR;
+  
+  delay_ms(100);
+ // eep_16bit_write(EEPADDR,0xbbbb);
+  eep_16bit_read(EEPADDR,&a);
+  buf=a;
+  
 
+  
+  
+  delay_ms(100);
 
-      NRF24L01_RX_Mode();
-      //Âí´ï¿ØÖÆ
-      while(1)
-      {
-        if(NRF24L01_RxPacket(rx_buff)==0)
-        {
-          Left_Run((s8)rx_buff[0]);
-          Right_Run((s8)rx_buff[1]);
-        }
-       delay_ms(1);
-      }
-      
+  
   while(1)
   {
-    if(NRF24L01_RxPacket(rx_buff)==0)
+    t = ADC_Data();
+//    buf=t;
+    
+
+    if(t>400)
     {
-        R = rx_buff[0];
-	  G = rx_buff[1];
-	  B = rx_buff[2];
-          
+      LED_R_SET;
+      eep_16bit_write(EEPADDR,0xdddd);
+      LED_R_CLR;
     }
-    if(RL<R) RL++;
-          else if(RL>R) RL--;
-          if(GL<G) GL++;
-          else if(GL>G) GL--;
-          if(BL<B) BL++;
-          else if(BL>B) BL--;
-    delay_ms(1);
-    
-    
-//    if(NRF24L01_RxPacket(rx_buff)==0)
-//    {
-//      if(rx_buff[0]&0x01) LED_R_CLR;
-//      else LED_R_SET;
-//      if(rx_buff[0]&0x02) LED_G_CLR;
-//      else LED_G_SET;
-//      if(rx_buff[0]&0x04) LED_B_CLR;
-//      else LED_B_SET;
-//      
-//    }else delay_ms(100);
-      //NRF24L01_TxPacket("XSSR");
-	 // NRF24L01_TxPacket("Ccccccccccc");
-	//	  delay_ms(1000);
-  }              
+//    delay_ms(2);
+  }
+  
+
 }
 void All_Congfig(void)
 {
